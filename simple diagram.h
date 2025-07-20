34bc3cd80c7bcdd3364878bfa040d11a4544a2ec
Sibami <duongvanthoduong36@gmail.com>
2025-07-20 19:52:33 +0700

    Update simple diagram.h

--- simple diagram.h
+++ simple diagram.h
@@ -1,5 +1,5 @@
 this is a simple tv b gone wiring diagram if you want to understand more visit readme.h
-positive leg of ir led-100-150r resistor-3.7-4.2v
-negative leg of ir led-E transistor 2n2222
+anode leg of ir led-100-150r resistor-3.7-4.2v
+cathode leg of ir led-E transistor 2n2222
 transistor 2n2222 B-2k2resistor-d5 esp8266
 E-GND esp8266
